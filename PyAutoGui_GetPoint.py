8d1539f8ba71b4a89d69cd17990a7808381d588c
Administrator <516545865@qq.com>
2018-11-23 11:23:59 +0800

    合并了多项相似操作过程。

--- PyAutoGui_GetPoint.py
+++
@@ -1,8 +0,0 @@
-import pyautogui as pg
-
-try:
-    while True:
-        if pg.confirm('', '', ['OK', 'Cancel']) == 'OK':
-            print(pg.position())
-except KeyboardInterrupt:
-    print('/n')
